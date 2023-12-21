dd48f2ef472393985c9fd0c534da0272d8f7a24f
37un0 <127773358+brunin6951@users.noreply.github.com>
2023-12-21 19:24:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-console.log("testtste")
+let nomeHeroi = "o super dev"
+let xpHeroi = 102000
 
-console.log(10 * 10 * 10 )
+
+if(xpHeroi < 1000){
+    console.log("Seu Heroi é Ferro")
+}else if (xpHeroi >= 1000 && xpHeroi <= 2000) {
+    console.log("Seu Heroi é Bronze")
+}else if (xpHeroi >= 2001 && xpHeroi <= 5000){
+    console.log("Seu Heroi é Prata ")
+}else if (xpHeroi >= 5001 && xpHeroi <= 7000){
+    console.log("Seu Heroi é Ouro ")
+}else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+    console.log("Seu Heroi é Platina")
+}else if (xpHeroi >= 8001 && xpHeroi <=9000){
+    console.log("Seu Heroi é Ascendente")
+}else if (xpHeroi >= 9001 && xpHeroi <= 10000){
+    console.log("Seu Heroi é Imortal!")   
+}else {
+    console.log("Radiante")
+}
